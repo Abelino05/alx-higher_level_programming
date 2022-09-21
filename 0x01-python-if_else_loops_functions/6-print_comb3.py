89303fee9422439f275084d61fc452ab6e6ed59f
Abelino05 <pakpaliabel@gmail.com>
2022-09-21 19:34:35 +0000

    Task 6 fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 
 for i in range(0, 9):
     for j in range(i+1, 10):
-        if i != j:
-            print(f'{i:d}{j:d}', end=', ')
+        if i != j and i != 8:
+            print('{:d}{:d}'.format((i), (j)), end=', ')
+        else:
+            print('{:d}{:d}'.format((i), (j)))

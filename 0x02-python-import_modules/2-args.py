fa1c58f874cf366ad7127c79baf5d77a3f1a9bf5
Abelino05 <pakpaliabel@gmail.com>
2022-09-24 23:14:57 +0000

    My arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,16 +3,21 @@
 import sys
 
 
-lenght = len(sys.argv[1:])
-arguments = list(sys.argv)
-if lenght == 0:
-    print('0 arguments.')
-elif lenght == 1:
-    print('1 argument:')
-    print('1: {}'.format(arguments[1]))
-elif lenght > 1:
-    print('{} arguments:'.format(lenght))
-    for i in range(1, lenght + 1):
-        print('{}: {:}'.format(i, arguments[i]))
-else:
-    pass
+def my_arguments():
+    lenght = len(sys.argv[1:])
+    arguments = list(sys.argv)
+    if lenght == 0:
+        print('0 arguments.')
+    elif lenght == 1:
+        print('1 argument:')
+        print('1: {}'.format(arguments[1]))
+    elif lenght > 1:
+        print('{:d} arguments:'.format(lenght))
+        for i in range(1, lenght + 1):
+            print('{:d}: {:s}'.format(i, arguments[i]))
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    my_arguments()

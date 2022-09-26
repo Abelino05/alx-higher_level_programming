64ce5366acab2a51a468c8a50fc5273c35250cff
Abelino05 <pakpaliabel@gmail.com>
2022-09-26 19:31:04 +0000

    Range fixed but not resolved

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,11 +2,12 @@
 
 
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-    for i in range(0, len(my_list)):
+   # new_list = my_list.copy()
+    for i in range(0, (len(my_list) - 1)):
         for element in my_list:
+            new_list = my_list.copy()
             if element % 2 == 0:
                 new_list[i] = True
             else:
                 new_list[i] = False
-        return new_list
+            return new_list

9634402cb27c7cdb91a5a85f3d966412055c4956
Abelino05 <pakpaliabel@gmail.com>
2022-09-26 18:34:14 +0000

    Replace elements in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        new_list = my_list
+        return new_list

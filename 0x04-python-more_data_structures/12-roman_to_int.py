62bed1c817c929b0a250b886f24519e6e8534dda
Abelino05 <pakpaliabel@gmail.com>
2022-09-30 19:25:41 +0000

    Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str) is False or roman_string is None:
+        return 0
+    s = roman_string[:]
+    roman_int = 0
+    i = 0
+    for i in range(0, len(s)):
+        if s[i] == 'I':
+            if i < len(s) - 1:
+                if s[i + 1] in ['V', 'X']:
+                    roman_int -= 1
+                else:
+                    roman_int += 1
+            else:
+                roman_int += 1
+        elif s[i] == 'V':
+            roman_int += 5
+        elif s[i] == 'X':
+            if i < len(s) - 1:
+                if s[i + 1] in 'LMC':
+                    roman_int -= 10
+                else:
+                    roman_int += 10
+            else:
+                roman_int += 10
+        elif s[i] == 'L':
+            roman_int += 50
+        elif s[i] == 'C':
+            roman_int += 100
+        elif s[i] == 'D':
+            roman_int += 500
+        elif s[i] == 'M':
+            roman_int += 1000
+        i += 1
+    return roman_int

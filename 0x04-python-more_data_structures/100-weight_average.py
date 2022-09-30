732e362d6cbea1681f0abf9ea21053367a08602f
Abelino05 <pakpaliabel@gmail.com>
2022-09-30 19:27:08 +0000

    Advanced tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    numerator = 0
+    denominator = 0
+    for tup in my_list:
+        numerator += (tup[0] * tup[1])
+    for tup in my_list:
+        denominator += tup[1]
+    return numerator / denominator

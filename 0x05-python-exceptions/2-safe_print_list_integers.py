b0d280378d17691bab75e7d1c901580c1c7e3660
Abelino05 <pakpaliabel@gmail.com>
2022-10-10 21:34:33 +0000

    Prints anc count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+	num_element = 0
+	for i in range(x):
+		try:
+			print('{:d}'.format(my_list[i]), end='')
+			num_element += 1
+		except IndexError:
+			raise
+		except Exception:
+			continue
+	print('')
+	return num_element

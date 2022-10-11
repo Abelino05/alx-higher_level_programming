850c55bb3b7030c3a5f664f95907e7be31ff5eeb
Abelino05 <pakpaliabel@gmail.com>
2022-10-11 13:46:02 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains only one class"""
+
+
+class Square:
+    """Suare defined by private attribute size"""
+    def __init__(self, size=0):
+        self.__size = size

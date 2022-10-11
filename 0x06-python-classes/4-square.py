89ae519f473ee1ac9bc8b3da0d1181571a210577
Abelino05 <pakpaliabel@gmail.com>
2022-10-11 16:42:36 +0000

    Access an update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module contains class square"""
+
+
+class Square:
+    """	Calculate the square of a given size"""
+    def __init__(self, size=0):
+        """The constructor:
+        Attribute:
+            size: int parameter
+        """
+        self.__size = int(size)
+
+    def area(self):
+        """Calculate the square."""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """return square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size value"""
+        if isinstance(value, int) is not True:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value

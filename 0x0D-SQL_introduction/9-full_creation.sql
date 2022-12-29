3384181f2c4aa9c7c63469456b434f827d36e31f
Abelino05 <pakpaliabel@gmail.com>
2022-12-29 17:10:20 +0000

    Sql statement reviewed

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- creates a table and adds rows
-CREATE TABLE IF NOT EXISTS ´second_table´
-(´id´ INT,
-´name´ VARCHAR(256),
-´score´ INT);
-INSERT INTO `second_table` (´id´, ´name´, ´score´)
+CREATE TABLE IF NOT EXISTS second_table
+(id INT,
+name VARCHAR(256),
+score INT);
+INSERT INTO second_table (id, name, score)
 VALUES
 (1, "John", 10),
 (2, "Alex", 3),

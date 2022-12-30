2364378816d34dd7b6eacb809536eb07a5cdf695
Abelino05 <pakpaliabel@gmail.com>
2022-12-30 14:47:58 +0000

    Script reviewed

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of a table
-SELECT 'score', 'name' FROM 'second_table' WHERE 'name' != "" ORDER BY 'score' DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
